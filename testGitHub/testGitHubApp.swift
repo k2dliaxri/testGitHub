2ba039e614bee42dc6c195fa30f31c7787674bd7
k2dliaxri <k2dliaxri@hotmail.com>
2021-12-01 14:33:55 +0800

    Initial Commit

---
+++ testGitHub/testGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  testGitHubApp.swift
+//  testGitHub
+//
+//  Created by hying on 2021/12/1.
+//
+
+import SwiftUI
+
+@main
+struct testGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
